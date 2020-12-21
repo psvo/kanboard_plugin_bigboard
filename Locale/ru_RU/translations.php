d3d1567de3e8b69c9d22ca1c1dde7d4fa63b56ac
Timo <tili2@gmx.de>
2020-12-21 12:47:40 +0100

    #21 Russian language

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,16 @@
+<?php
+return [
+	'BigBoard' => 'Комплексная доска',
+	'manage project selection' => 'Управление отбором проектов',
+	'Collapse all projects' => 'Свернуть все проекты',
+	'Expand all projects' => 'Развернуть все проекты',
+	'favorites' => 'избранное',
+	'check' => 'выделять',
+	'uncheck' => 'отменить выбор',
+	'everything' => 'все',
+	'add' => 'добавить',
+	'only' => 'лишь',
+	'Project selection' => 'Выбранные проекты',
+	'no project has been selected yet for multiple view ; you can select some now : ' => 'Еще не выбран ни один проект для множественного просмотра; вы можете выбрать сейчас: ',
+	'options' => 'Настройка',
+];
